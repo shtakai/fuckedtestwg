76f6b673dd203a8b5fbb8e9f7f80b8da3c8ae1d1
Alyson Takai <shtakai@gmail.com>
2019-01-06 17:08:18 +0700

    Modify applying convention

--- example/greet.go
+++ example/greet.go
@@ -1,3 +1,4 @@
+// package example is fucked off
 package example
 
 import "fmt"
--- example/greet_test.go
+++ example/greet_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func ExampleHello() {
+func ExampleHello_spanish() {
 	greeting, err := Hello("Jon")
 	if err != nil {
 		panic(err)
