bbe01655dc37c011903c53678585e98a570df2fc
Alyson Takai <shtakai@gmail.com>
2019-01-06 17:01:51 +0700

    Add test code

---
+++ example/greet_test.go
@@ -0,0 +1,16 @@
+package example
+
+import (
+	"fmt"
+)
+
+func ExampleHello() {
+	greeting, err := Hello("Jon")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(greeting)
+
+	// Output:
+	// Fuck, Jon
+}
