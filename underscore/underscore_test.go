9e9f307fa69a5ae67e1ff23e37e20921cee76e45
Alyson Takai <shtakai@gmail.com>
2019-01-09 20:15:08 +0700

    Create test from test code generator

--- underscore/underscore_test.go
+++ underscore/underscore_test.go
@@ -3,20 +3,23 @@ package underscore
 import "testing"
 
 func TestCamel(t *testing.T) {
-	testCases := []struct {
-		arg  string
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
 		want string
 	}{
-		{"thisIsACamelCaseString", "this_is_a_camel_case_string"},
-		{"with a space", "with a space"},
-		{"endsWithA", "ends_with_a"},
+		{"simple", args{"thisIsACamelCaseString"}, "this_is_a_camel_case_string"},
+		{"spaces", args{"with a space"}, "with a space"},
+		{"ends with capital", args{"endsWithA"}, "ends_with_a"},
 	}
-
-	for _, tc := range testCases {
-		t.Logf("Testing: %q", tc.arg)
-		got := Camel(tc.arg)
-		if got != tc.want {
-			t.Errorf("Camel(%q) = %q; want %q", tc.arg, got, tc.want)
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Camel(tt.args.str); got != tt.want {
+				t.Errorf("Camel() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
