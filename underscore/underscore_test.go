07133de6857dfe6b4e25eab6cdf1a265d4530af9
Alyson Takai <shtakai@gmail.com>
2019-01-09 20:35:38 +0700

    Modify for understanding easily

--- underscore/underscore_test.go
+++ underscore/underscore_test.go
@@ -3,22 +3,18 @@ package underscore
 import "testing"
 
 func TestCamel(t *testing.T) {
-	type args struct {
-		str string
-	}
 	tests := []struct {
-		name string
-		args args
+		arg  string
 		want string
 	}{
-		{"simple", args{"thisIsACamelCaseString"}, "this_is_a_camel_case_string"},
-		{"spaces", args{"with a space"}, "with a space"},
-		{"ends with capital", args{"endsWithA"}, "ends_with_a"},
+		{"thisIsACamelCaseString", "this_is_a_camel_case_string"},
+		{"with a space", "with a space"},
+		{"endsWithA", "ends_with_a"},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Camel(tt.args.str); got != tt.want {
-				t.Errorf("Camel() = %v, want %v", got, tt.want)
+		t.Run(tt.arg, func(t *testing.T) {
+			if got := Camel(tt.arg); got != tt.want {
+				t.Fatalf("Camel() = %v, want %v", got, tt.want)
 			}
 		})
 	}
