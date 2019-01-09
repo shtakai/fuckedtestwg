3e89b91d0390c661d59a824a4c5dcaeed4d3ab60
Alyson Takai <shtakai@gmail.com>
2019-01-09 20:04:49 +0700

    Modify to table driven test

--- underscore/underscore_test.go
+++ underscore/underscore_test.go
@@ -2,12 +2,21 @@ package underscore
 
 import "testing"
 
-func TestCamel_simple(t *testing.T) {
-	arg := "thisIsACamelCaseString"
-	want := "this_is_a_camel_case_string"
-	got := Camel(arg)
+func TestCamel(t *testing.T) {
+	testCases := []struct {
+		arg  string
+		want string
+	}{
+		{"thisIsACamelCaseString", "this_is_a_camel_case_string"},
+		{"with a space", "with a space"},
+		{"endsWithA", "ends_with_a"},
+	}
 
-	if got != want {
-		t.Errorf("Camel(%q) = %q; want %q", arg, got, want)
+	for _, tc := range testCases {
+		t.Logf("Testing: %q", tc.arg)
+		got := Camel(tc.arg)
+		if got != tc.want {
+			t.Errorf("Camel(%q) = %q; want %q", tc.arg, got, tc.want)
+		}
 	}
 }
