6574d8cdf6b08384b2830530d47adc19131c4fc7
Alyson Takai <shtakai@gmail.com>
2019-01-13 16:48:17 +0700

    Add test with tags

---
+++ skip/tag_mysql_test.go
@@ -0,0 +1,9 @@
+// +build mysql
+
+package skip
+
+import "testing"
+
+func TestMySQL(t *testing.T) {
+	t.Log("Testing mysql stuff")
+}
---
+++ skip/tag_psql_test.go
@@ -0,0 +1,9 @@
+// +build psql
+
+package skip
+
+import "testing"
+
+func TestPostgreSQL(t *testing.T) {
+	t.Log("Testing pgsql stuff")
+}
