918cfb0bd534ec2fdd026f7050d2ec02e36e7871
Alyson Takai <shtakai@gmail.com>
2019-01-20 17:41:47 +0700

    Add another DI testing w/ git

---
+++ git/git.go
@@ -0,0 +1,43 @@
+package git
+
+import (
+	"os/exec"
+	"strings"
+)
+
+// approach1: global
+var execCommand = exec.Command
+
+func Version() string {
+	cmd := execCommand("git", "version")
+	stdout, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	n := len("git version ")
+	version := string(stdout[n:])
+	return strings.TrimSpace(version)
+}
+
+// approach2: make more sense
+type Checker struct {
+	execCommand func(name string, arg ...string) *exec.Cmd
+}
+
+func (gc *Checker) command(name string, arg ...string) *exec.Cmd {
+	if gc.execCommand == nil {
+		return exec.Command(name, arg...)
+	}
+	return gc.execCommand(name, arg...)
+}
+
+func (gc *Checker) Version() string {
+	cmd := gc.command("git", "version")
+	stdout, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	n := len("git version ")
+	version := string(stdout[n:])
+	return strings.TrimSpace(version)
+}
