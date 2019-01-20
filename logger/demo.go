59e60edccdd037f0b071211846acc2920fd1552e
Alyson Takai <shtakai@gmail.com>
2019-01-20 15:53:39 +0700

    Add di implementation agnostic code

--- logger/demo.go
+++ logger/demo.go
@@ -21,6 +21,49 @@ func DemoV2(logger *log.Logger) {
 	}
 }
 
+// Call this w/ :
+// No longer care about logger
+//  logger := log.New(...)
+//  DemoV3(log.Println)
+func DemoV3(logFn func(...interface{})) {
+	err := doTheThing()
+	if err != nil {
+		logFn("error in doTheThing():", err)
+	}
+}
+
+// For DemoV4
+type Logger interface {
+	Println(...interface{})
+	Printf(string, ...interface{})
+}
+
+// Call this w/
+//  logger := log.New(...)
+//  DemoV4(logger)
+func DemoV4(logger Logger) {
+	err := doTheThing()
+	if err != nil {
+		logger.Println("error in doTheThing():", err)
+		logger.Printf("error: %s\n", err)
+	}
+}
+
+type Thing struct {
+	Logger interface {
+		Println(...interface{})
+		Printf(string, ...interface{})
+	}
+}
+
+func (t Thing) DemoV5() {
+	err := doTheThing()
+	if err != nil {
+		t.Logger.Println("error in doTheThing():", err)
+		t.Logger.Println("error: %s\n", err)
+	}
+}
+
 func doTheThing() error {
 	return errors.New("error opening file: abc.txt")
 }
